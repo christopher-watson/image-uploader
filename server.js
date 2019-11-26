3a4da359cb56449b65f53fd8335d5bb786a63fd5
Christopher Watson <christopher_watson@icloud.com>
2019-11-25 21:29:05 -0500

    files and ignore

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const mongoose = require('mongoose');
+
+
+// Set up Express
+const app = express();
+const PORT = process.env.PORT || 3030;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev')); // for logging
+
+// Import routes
+const routes = require('./server/routes');
+app.use(routes);
+
+// DB Config
+const keys = require("./config/keys");
+
+// Connect to MongoDB
+// mongoose.connect(keys.mongoURI,{ useNewUrlParser: true, dbName: 'lwatson-db' })
+//   .then(() => console.log("💾  ==> DB successfully connected"))
+//   .catch(err => console.log(err));
+
+// Start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}`);
+});
+
