e418ab87e7f38582b72b47b75c8cf21a8f334f9d
Christopher Watson <christopher_watson@icloud.com>
2019-11-30 13:23:02 -0500

    toggle

--- public/script.js
+++ public/script.js
@@ -1,11 +1,27 @@
 // DOM elements
 const tbody = document.getElementById('tbody');
 
+// toggle name
+toggleName = data => {
+  if (data.target.innerText === data.target.getAttribute('name')) {
+    data.target.innerText = data.target.getAttribute('mail');
+    data.target.setAttribute('class', 'text-muted');
+  } else {
+    data.target.innerText = data.target.getAttribute('name');
+    data.target.removeAttribute('class', 'text-muted');
+  }
+};
+
 // new table row
-let trow = text => {
+let trow = (name, email) => {
   let row = document.createElement('tr');
   let data = document.createElement('td');
-  data.innerText = text;
+  data.innerText = name;
+  data.setAttribute('name', name);
+  data.setAttribute('mail', email);
+  data.addEventListener('click', e => {
+    toggleName(e);
+  });
   row.appendChild(data);
   return row;
 };
@@ -20,12 +36,11 @@ getAllUsers = () => {
       data = await res.data;
       for (var i of data) {
         await tbody.appendChild(
-          trow(i.name.charAt(0).toUpperCase() + i.name.slice(1))
+          trow(i.name.charAt(0).toUpperCase() + i.name.slice(1), i.email)
         );
       }
     });
 };
-
 getAllUsers();
 
 // add new user to db
