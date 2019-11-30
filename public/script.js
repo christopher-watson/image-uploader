9fcd2fed1d5cdd09002c26390d97585dece67282
Christopher Watson <christopher_watson@icloud.com>
2019-11-30 14:58:23 -0500

    api stuff

--- public/script.js
+++ public/script.js
@@ -63,3 +63,24 @@ submit.addEventListener('click', async () => {
   email.value = '';
   name.value = '';
 });
+
+// toggle api div
+let pressed = [];
+const secretCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+window.addEventListener('keyup', e => {
+  pressed.push(e.keyCode);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (JSON.stringify(pressed) === JSON.stringify(secretCode)) {
+    toggleApiDiv();
+  }
+});
+
+toggleApiDiv = () => {
+  const apiDiv = document.getElementById('api-container');
+  if(apiDiv.style.display === 'none'){
+    apiDiv.setAttribute('style', 'display: initial')
+  }
+  else{
+    apiDiv.setAttribute('style', 'display: none')
+  }
+};
