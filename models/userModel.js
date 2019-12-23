82e8366ad507b5d6fde6d19da8ceb87947ffbcb6
Christopher Watson <christopher_watson@icloud.com>
2019-12-23 15:27:54 -0500

    update user model 11

--- models/userModel.js
+++ models/userModel.js
@@ -10,13 +10,13 @@ const Users = new Schema({
     type: String,
     default: 'testEmail'
   },
-  _images: [String],
-  // _images: [
-  //   {
-  //     // type: Schema.Types.Mixed,
-  //     ref: 'Images'
-  //   }
-  // ]
+  // _images: [String],
+  _images: [
+    {
+      type: Schema.Types.Mixed,
+      ref: 'Images'
+    }
+  ]
 });
 
 module.exports = mongoose.model('Users', Users);
