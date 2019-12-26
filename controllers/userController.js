f55b486292604ff425796342505f3593691c15eb
Christopher Watson <christopher_watson@icloud.com>
2019-12-25 22:09:10 -0500

    add image at index

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,6 +48,24 @@ module.exports = {
     )
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+  addImageAtIndex: function(req, res) {
+    User.findOneAndUpdate(
+      // email as parameter
+      { email: req.params.id },
+      // image id as body
+      {
+        $push: {
+          _images: {
+            $each: [req.body],
+            $position: req.params.pos
+          }
+        }
+      }
+      // {safe: true, upsert: true},
+    )
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
   // updateUser: function(req, res) {
   //   User.findOneAndUpdate(
